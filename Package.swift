08f5852e05e0fee90526e586635ab899542ba48a
kyle.kao <kyle.kao@kakaocorp.com>
2021-07-02 11:23:22 +0900

    수정

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "KakaoAdPackage",
+    platforms: [
+        .iOS(.v9)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -26,8 +29,5 @@ let package = Package(
             dependencies: []),
         .binaryTarget(name: "KakaoAdSDK",
                       path: "Framework/KakaoAdSDK.xcframework"),
-        .testTarget(
-            name: "KakaoAdPackageTests",
-            dependencies: ["KakaoAdPackage"]),
     ]
 )
